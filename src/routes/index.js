524b3154c156a0c6e700546dad3b3a83ba81eb58
Thuy trinh ne <21522719@gm.uit.edu.vn>
2024-11-03 11:51:04 +0700

    feat(auth) remove test auth

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,13 +3,12 @@ import guestRouter from "./guest/index.js";
 import cloudinaryRouter from "./cloudinary/index.js";
 import authRouter from "./auth/index.js";
 import passport from '../passport.js';
-import { verifyToken, verifyTokenAndAdmin } from '../middlewares/auth.middleware.js';
 
 const route = (app) => {
   app.get("/", (req, res) => res.send("Express on Vercel"));
-  app.use("/api/admin", verifyTokenAndAdmin, adminRouter);
+  app.use("/api/admin", adminRouter);
   app.use("/api/guest", guestRouter);
-  app.use("/api/cloudinary", verifyToken, cloudinaryRouter);
+  app.use("/api/cloudinary", cloudinaryRouter);
   app.use("/api/auth", authRouter);
   // init passport
   if (process.env.GOOGLE_CLIENT_ID && process.env.GOOGLE_CLIENT_SECRET && process.env.GOOGLE_CALLBACK_URL) {
