06d0fbfd2521a0f28c575ef66fa18c92e8909afb
ldmnh <leducmanh123kt@gmail.com>
2024-11-10 17:10:48 +0700

    feat(admin/coupons + admin/groups): add admin coupons and groups

--- src/models/coupon.model.js
+++ src/models/coupon.model.js
@@ -4,15 +4,14 @@ const couponSchema = new mongoose.Schema(
   {
     coupon_name: { type: String, required: true },
     coupon_description: { type: String },
-    coupon_type: { type: String, required: true },
-    coupon_condition: { type: Number, required: true },
-    coupon_unit: { type: String, required: true },
-    coupon_value: { type: Number, required: true },
-    coupon_max_value: { type: String },
-    coupon_numbers: { type: Number, required: true },
-    start_time: { type: Date, required: true },
-    end_time: { type: Date, required: true },
-    isAll: { type: Boolean, required: true },
+    coupon_type: { type: String, enum: ["Free Ship", "Order"], default: "Order", required: true }, // Loại mã: Freeship hoặc Trừ tiền
+    coupon_condition: { type: Number, required: true }, // Giảm khi đơn lớn hơn m tiền (áp dụng cho tất cả khi điều kiện = 0)
+    coupon_unit: { type: String, enum: ["%", "đ"], default: "đ", required: true }, // Đơn vị giảm % hoặc đ
+    coupon_value: { type: Number, required: true }, // Giá trị giảm
+    coupon_max_value: { type: Number }, // Giá trị giảm tối đa (áp dụng cho mã theo %)
+    coupon_stock_quantity: { type: Number, required: true }, // Số lượng mã giảm
+    start_time: { type: Date, required: true }, // Thời gian mở lấy mã
+    end_time: { type: Date, required: true }, // Thời gian đóng lấy mã
   },
   { timestamps: true }
 );
