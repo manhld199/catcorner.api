8d911a69eeaeec1ed28c83fc67615afdfd0218d0
ldmnh <leducmanh123kt@gmail.com>
2024-12-17 20:08:30 +0700

    feat(guest/category): add get categories

--- src/routes/guest/index.js
+++ src/routes/guest/index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import productListRoutes from "./productList.route.js";
 import productRoutes from "./product.route.js";
+import categoryRouter from "./category.route.js";
 
 const router = express.Router();
 
 router.use("/product", productRoutes);
 router.use("/productList", productListRoutes);
+router.use("/categories", categoryRouter);
 
 export default router;
