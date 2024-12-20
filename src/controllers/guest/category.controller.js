89099906030af03824ddfe737442ee08758a917c
Phan Nguyen Hai Yen <pnhaiyen520@gmail.com>
2024-12-20 07:34:50 +0700

    fix api/guest/categories

--- src/controllers/guest/category.controller.js
+++ src/controllers/guest/category.controller.js
@@ -4,21 +4,16 @@ import Category from "../../models/category.model.js";
 import { notFound, ok, error, created } from "../../handlers/respone.handler.js";
 import { encryptData, decryptData } from "../../utils/security.js";
 
-// [GET] / api/admin/categories
+// [GET] / api/guest/categories
 export const getCategories = async (req, res, next) => {
   try {
     const categories = await Category.aggregate([{ $match: {} }, { $sort: { createdAt: -1 } }]);
 
-    // Thêm category_id_hashed bằng cách áp dụng hàm encryptData
-    const transformedCatrgories = categories.map((category) => ({
-      ...category,
-      category_id_hashed: encryptData(category._id.toString()),
-    }));
     // console.log("products: ", products);
 
-    if (!transformedCatrgories.length) return notFound(res, {});
+    if (!categories.length) return notFound(res, {});
 
-    return ok(res, transformedCatrgories);
+    return ok(res, { categories: categories });
   } catch (err) {
     console.log("Err: " + err);
     return error(res);
