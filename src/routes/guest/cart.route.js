44670affb6acbdebc1fda8271ec49c7ac85bbc65
Phan Nguyen Hai Yen <pnhaiyen520@gmail.com>
2024-12-27 17:22:39 +0700

    delete /guest/cart

--- src/routes/guest/cart.route.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-import { getCartProducts } from "../../controllers/guest/cart.controller.js";
-
-const router = express.Router();
-
-router.get("/", getCartProducts);
-
-export default router;
